e7f1b2b485d17e6b924426b019be418c8ca71b8f
Kaheridon <alexandre.vivierbaudry@viacesi.fr>
2018-05-30 16:07:02 +0200

    Modification " " into "."

--- lorann/model/src/main/java/motionless/Floor.java
+++ lorann/model/src/main/java/motionless/Floor.java
@@ -5,7 +5,7 @@ import Element.Sprite;
 
 class Floor extends MotionlessElement {
 	/** The Constant SPRITE. */
-	private static final Sprite SPRITE = new Sprite(' ',"floor.png"); // /!\ Warning : ne pas oublier de cr�er le sprite "floor.png" (il s'agit d'un simple fond noir) /!\
+	private static final Sprite SPRITE = new Sprite('.',"floor.png"); 
 	/** Instantiates a new Floor. */
 	Floor(){
 		super(SPRITE, Permeability.BLOCKING);
