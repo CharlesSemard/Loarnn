6824b0bd7ddc6b255eecc6fb488b32acaa611c77
Steinwolf <pierreloup.martigne@viacesi.fr>
2018-05-30 09:31:25 +0200

    added packages
    created "packages" and "packages-info.java" for "mobile", "motionless" and "element"

--- lorann/model/src/main/java/motionless/package-info.java
+++ lorann/model/src/main/java/motionless/package-info.java
@@ -1,9 +1,9 @@
 /**
- * Provides all classes for the element component.
+ * Provides all classes for the motionless component.
  * 
  * @author Pierre-Loup MARTIGNE pierreloup.martigne@viacesi.fr
  * 
  * @version 1.0
  *
  */
-package element;
+package motionless;
