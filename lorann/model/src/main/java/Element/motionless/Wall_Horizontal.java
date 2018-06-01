8908876e24e9467b35cf0219b590236ac02adebc
GwendalLupart <gwendal.lupart@viacesi.fr>
2018-06-01 14:19:57 +0200

    Auto stash before merge of "try_alex" and "origin/try_alex"

--- lorann/model/src/main/java/Element/motionless/Wall_Horizontal.java
+++ lorann/model/src/main/java/Element/motionless/Wall_Horizontal.java
@@ -1,9 +1,14 @@
 package Element.motionless;
 
 import java.awt.Image;
+<<<<<<< Updated upstream:lorann/model/src/main/java/Element/motionless/Wall_Horizontal.java
 
 import model.Permeability;
 import model.Sprite;
+=======
+import Element.Permeability;
+import Element.Sprite;
+>>>>>>> Stashed changes:lorann/model/src/main/java/motionless/Wall_Horizontal.java
 
 class Wall_Horizontal extends MotionlessElement {
 	
